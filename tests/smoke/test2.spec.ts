0a8fbf3a4ab198fce379210b4d557cf36cad9fbf
pri <priyanka.jadhav060791@gmail.com>
2024-08-03 12:35:45 -0400

    updated tests files

--- tests/smoke/test2.spec.ts
+++ tests/smoke/test2.spec.ts
@@ -5,7 +5,7 @@ test('Failed Login test',async({page})=>{
     await page.goto('https://react-redux.realworld.io/')
     await page.locator('//a[normalize-space()="Sign in"]').click()
     await page.locator('//input[@placeholder="Email"]').fill('playwrightdemo@gmail.com')
-    await page.locator('//input[@placeholder="fuygjgkPassword"]').fill('playwrightdemo')
+    await page.locator('//input[@placeholder="Password"]').fill('playwrightdemo')
     await page.locator('//button[normalize-space()="Sign in"]').click();
     
 })
